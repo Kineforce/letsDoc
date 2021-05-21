272be1424c3efb5b168d7bdf333a0328c9f86bd2
kineforce <lucas-sm2010@hotmail.com>
2021-05-21 01:08:43 -0300

    Melhorias no layout

--- index.php
+++ index.php
@@ -44,7 +44,6 @@
         </div>
     </main>
     
-    
 
     <script src="https://code.jquery.com/jquery-3.6.0.slim.min.js" integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" crossorigin="anonymous"></script>
     <script src="./public/js/scripts.js"></script>
