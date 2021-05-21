85e94936382ace73df67eca7af44e4d489337626
kineforce <lucas-sm2010@hotmail.com>
2021-05-21 00:00:03 -0300

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # letsDoc
+
 Aplicação escrita em PHP para ajudar na documentação de sistemas
+
+# tecnologias
+
+PHP, Javascript/Jquery, HTML, CSS
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"></script>
+    <title>letsDoc</title>
+</head>
+<body>
+    
+    <header class="navbar navbar-expand navbar-dark flex-column flex-md-row bd-navbar">
+        <div class="navbar-nav-scroll">
+            <ul class="navbar-nav bd-navbar-nav flex-row">
+            </ul>
+        </div>
+    </header>
+    <main>Main</main>
+    <footer>Footer</footer>
+
+
+    <script src="./public/js/scripts.js"></script>
+</body>
+</html>
+
+
+
